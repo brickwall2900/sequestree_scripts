787eb84c0f72887ba2c321c65b402eaa13d657f3
brickwall2900 <brickwall2900@gmail.com>
2025-10-21 13:15:48 +0800

    rename components

---
+++ rasters_view.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+def run(title, rasters):
+    import streamlit as st
+    import leafmap.foliumap as foliumap
+    import folium
+    from branca.colormap import linear
+    from leafmap.foliumap import SplitControl
+
+    st.set_page_config(layout="wide")
+    st.title(title)
+
+    biomass_rasters = rasters
+
+    col1, col2 = st.columns(2)
+    with col1:
+        left_year = st.selectbox("Left Map (Year)", list(biomass_rasters.keys()), index=0)
+    with col2:
+        right_year = st.selectbox("Right Map (Year)", list(biomass_rasters.keys()), index=len(biomass_rasters)-1)
+
+    m = foliumap.Map(center=[14.65, 121.05], zoom=12, basemap=None, tiles=None)
+
+    m.split_map(
+        left_layer=biomass_rasters[left_year],
+        right_layer=biomass_rasters[right_year],
+        left_args={'palette': 'Greens', 'vmin': 0, 'vmax': 2500},
+        right_args={'palette': 'Greens', 'vmin': 0, 'vmax': 2500},
+    )
+    colormap = linear.Greens_09.scale(0, 2000)
+    colormap.caption = "Aboveground Biomass (kg)"
+
+    m.add_colorbar(colors=colormap.colors, vmin=0, vmax=2500, caption="Aboveground Biomass (kg)")
+
+    m.to_streamlit(height=600)
+
