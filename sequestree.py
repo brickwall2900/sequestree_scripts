e5bd434a779a241f0484ae071c68f1add795e86d
brickwall2900 <brickwall2900@gmail.com>
2025-10-21 14:59:01 +0800

    the first iteration of the better sidebar

--- sequestree.py
+++ sequestree.py
@@ -4,7 +4,8 @@
 # In[2]:
 
 import streamlit as st
-import importlib
+from data_resolver import DataResolver, LocalDataResolver
+import rasters_view
 
 st.title("Spatial Assessment and Backcasting of Tree Carbon Sequestration (CS) in Quezon City, Philippines")
 
@@ -19,25 +20,46 @@ choices = [
     "Tree Carbon Sequestration Potential of Quezon City, Philippines Per Zone - Geographically Weighted Regression Predictions"
 ]
 
-choice = st.sidebar.selectbox("Select map to display:", choices)
+# options
+data_resolver = DataResolver(LocalDataResolver())
 
-module_map = {
-    choices[0]: "visualization",
-    choices[1]: "visualization_copy",
-    choices[2]: "visualization_copy_copy",
-    choices[3]: "visualization_copy_copy_copy",
-    choices[4]: "zonal",
-    choices[5]: "zonal_copy",
-    choices[6]: "zonal_copy_copy",
-    choices[7]: "zonal_copy_copy_copy"
+DATA_TYPE_MAP = {
+    "AGB": "Aboveground Biomass",
+    "POTENTIAL": "Tree Carbon Sequestration Potential"
 }
 
-module_name = module_map.get(choice)
-if module_name:
-    thing = importlib.import_module(module_name)
-    if hasattr(thing, "show_stuff"):
-        thing.show_stuff()
+MODEL_MAP = {
+    "RF": "Random Forest",
+    "HYBRID": "Hybrid"
+}
+
+OPTION_MAP = {
+    "RASTERS": "Raw Maps",
+    "VECTORS": "Zonal Maps"
+}
+
+def map_to_value(dict):
+    def _do_stuff(input):
+        return dict[input]
+    return _do_stuff
+
+YEARS = ["2020", "2021", "2022", "2023", "2024"]
+
+option_selected = st.sidebar.selectbox("Choose:", list(OPTION_MAP.keys()), format_func=map_to_value(OPTION_MAP))
+if option_selected == "RASTERS":
+    # there are different sidebar options
+    # above ground biomass, tree cabon seq potential
+    data_type = st.sidebar.selectbox("Data type", list(DATA_TYPE_MAP.keys()), format_func=map_to_value(DATA_TYPE_MAP))
+    model = st.sidebar.selectbox("Model", list(MODEL_MAP.keys()), format_func=map_to_value(MODEL_MAP))
+
+    folder_name = f"{option_selected}/{data_type}_{model}/"
+
+    ext = ".tif"
+    result = {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
+
+    if data_type == "AGB":
+        rasters_view.run("title here", result, "Aboveground Biomass (kg)", 0, 2500)
+    elif data_type == "POTENTIAL":
+        rasters_view.run("title here", result, "Tree Carbon Sequestration Potential", 0, 2)
     else:
-        st.error("Not implemented!")
-else:
-    st.error("Selected option not found in module map. This should NOT happen!")
+        raise TypeError("Unknown data type: " + data_type)
