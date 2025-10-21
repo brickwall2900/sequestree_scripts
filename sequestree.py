ad1d678b5014635fff8c8a6d19c8c73fde414ea0
brickwall2900 <brickwall2900@gmail.com>
2025-10-21 16:57:28 +0800

    second iteration: adapted vectors_view

--- sequestree.py
+++ sequestree.py
@@ -6,6 +6,7 @@
 import streamlit as st
 from data_resolver import DataResolver, LocalDataResolver
 import rasters_view
+import vectors_view
 
 st.title("Spatial Assessment and Backcasting of Tree Carbon Sequestration (CS) in Quezon City, Philippines")
 
@@ -23,8 +24,14 @@ choices = [
 # options
 data_resolver = DataResolver(LocalDataResolver())
 
-DATA_TYPE_MAP = {
+AGB_DATA_TYPE_MAP = {
     "AGB": "Aboveground Biomass",
+    "POTENTIAL": "Tree Carbon Sequestration Potential"
+}
+
+ZONE_DATA_TYPE_MAP = {
+    "AGB": "Tree Biomass and Carbon Stock",
+    "AGB_DENSITY": "Tree Biomass and Carbon Stock Density",
     "POTENTIAL": "Tree Carbon Sequestration Potential"
 }
 
@@ -33,9 +40,52 @@ MODEL_MAP = {
     "HYBRID": "Hybrid"
 }
 
+VIEW_TYPE_MAP = {
+    "BRGY": "Barangay",
+    "DIST": "District"
+}
+
 OPTION_MAP = {
     "RASTERS": "Raw Maps",
     "VECTORS": "Zonal Maps"
+}
+
+AGB_FIELDS = {
+    "average_agb": "Average AGB (kg)",
+    "standard_deviation": "Standard Deviation (kg)",
+    "total_agb": "Total AGB (kg)",
+    "total_bgb": "Total BGB (kg)",
+    "total_agb_c_stock": "Total AGB C Stock (kg)",
+    "total_bgb_c_stock": "Total BGB C Stock (kg)",
+    "total_agb_co2_stock": "Total AGB CO₂ Stock (kg)",
+    "total_bgb_co2_stock": "Total BGB CO₂ Stock (kg)"
+}
+
+POTENTIAL_FIELDS = {
+    "mean": "Mean (kg)",
+    "standard_deviation": "Standard Deviation (kg)"
+}
+
+AGB_DENSITY_FIELDS = {
+    "total_agb_density": "Total AGB Density (Mg/ha)",
+    "total_bgb_density": "Total BGB Density (Mg/ha)",
+    "total_agb_c_stock_density": "Total AGB C Stock Density (Mg/ha)",
+    "total_bgb_c_stock_density": "Total BGB C Stock Density (Mg/ha)",
+    "total_agb_co2_stock_density": "Total AGB CO₂ Stock Density (Mg/ha)",
+    "total_bgb_co2_stock_density": "Total BGB CO₂ Stock Density (Mg/ha)"
+}
+
+VECTOR_FIELDS = {
+    "BRGY": {
+        "AGB": {"barangay": "Barangay"} | AGB_FIELDS,
+        "POTENTIAL": {"barangay": "Barangay"} | POTENTIAL_FIELDS,
+        "AGB_DENSITY": {"barangay": "Barangay"} | AGB_DENSITY_FIELDS,
+    },
+    "DIST": {
+        "AGB": {"district": "District"} | AGB_FIELDS,
+        "POTENTIAL": {"district": "District"} | POTENTIAL_FIELDS,
+        "AGB_DENSITY": {"district": "District"} | AGB_DENSITY_FIELDS,
+    }
 }
 
 def map_to_value(dict):
@@ -49,7 +99,7 @@ option_selected = st.sidebar.selectbox("Choose:", list(OPTION_MAP.keys()), forma
 if option_selected == "RASTERS":
     # there are different sidebar options
     # above ground biomass, tree cabon seq potential
-    data_type = st.sidebar.selectbox("Data type", list(DATA_TYPE_MAP.keys()), format_func=map_to_value(DATA_TYPE_MAP))
+    data_type = st.sidebar.selectbox("Data Type", list(AGB_DATA_TYPE_MAP.keys()), format_func=map_to_value(AGB_DATA_TYPE_MAP))
     model = st.sidebar.selectbox("Model", list(MODEL_MAP.keys()), format_func=map_to_value(MODEL_MAP))
 
     folder_name = f"{option_selected}/{data_type}_{model}/"
@@ -58,8 +108,31 @@ if option_selected == "RASTERS":
     result = {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
 
     if data_type == "AGB":
-        rasters_view.run("title here", result, "Aboveground Biomass (kg)", 0, 2500)
+        rasters_view.run(result, "Aboveground Biomass (kg)", 0, 2500)
     elif data_type == "POTENTIAL":
-        rasters_view.run("title here", result, "Tree Carbon Sequestration Potential", 0, 2)
+        rasters_view.run(result, "Tree Carbon Sequestration Potential", 0, 2)
     else:
         raise TypeError("Unknown data type: " + data_type)
+elif option_selected == "VECTORS":
+    data_type = st.sidebar.selectbox("Data Type", list(ZONE_DATA_TYPE_MAP.keys()), format_func=map_to_value(ZONE_DATA_TYPE_MAP))
+    view_type = st.sidebar.selectbox("View Type", list(VIEW_TYPE_MAP.keys()), format_func=map_to_value(VIEW_TYPE_MAP))
+    model = st.sidebar.selectbox("Model", list(MODEL_MAP.keys()), format_func=map_to_value(MODEL_MAP))
+
+    folder_name = f"{option_selected}/{data_type}_{model}_{view_type}/"
+
+    ext = ".gpkg"
+    result = {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
+
+    fields = VECTOR_FIELDS[view_type][data_type]
+    field_colored = ""
+    match data_type:
+        case "AGB":
+            field_colored = "total_agb"
+        case "POTENTIAL":
+            field_colored = "mean"
+        case "AGB_DENSITY":
+            field_colored = "total_agb_density"
+        case _:
+            raise TypeError("Should NOT happen!")
+
+    vectors_view.run(result, field_colored, fields)
