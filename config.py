7eb6dc4602192179862849902f5cb18d624e632a
brickwall2900 <brickwall2900@gmail.com>
2025-10-21 17:15:21 +0800

    separate some data to separate file

---
+++ config.py
@@ -0,0 +1,81 @@
+YEARS = ["2020", "2021", "2022", "2023", "2024"]
+
+AGB_DATA_TYPE_MAP = {
+    "AGB": "Aboveground Biomass",
+    "POTENTIAL": "Tree Carbon Sequestration Potential"
+}
+
+AGB_DATA_TYPE_MIN_MAX = {
+    "AGB": (0, 2500),
+    "POTENTIAL": (0, 2)
+}
+
+AGB_DATA_TYPE_CAPTION = {
+    "AGB": "Aboveground Biomass (kg)",
+    "POTENTIAL": "Tree Carbon Sequestration Potential"
+}
+
+ZONE_DATA_TYPE_MAP = {
+    "AGB": "Tree Biomass and Carbon Stock",
+    "AGB_DENSITY": "Tree Biomass and Carbon Stock Density",
+    "POTENTIAL": "Tree Carbon Sequestration Potential"
+}
+
+MODEL_MAP = {
+    "RF": "Random Forest",
+    "HYBRID": "Hybrid"
+}
+
+VIEW_TYPE_MAP = {
+    "BRGY": "Barangay",
+    "DIST": "District"
+}
+
+OPTION_MAP = {
+    "RASTERS": "Raw Maps",
+    "VECTORS": "Zonal Maps"
+}
+
+AGB_FIELDS = {
+    "average_agb": "Average AGB (kg)",
+    "standard_deviation": "Standard Deviation (kg)",
+    "total_agb": "Total AGB (kg)",
+    "total_bgb": "Total BGB (kg)",
+    "total_agb_c_stock": "Total AGB C Stock (kg)",
+    "total_bgb_c_stock": "Total BGB C Stock (kg)",
+    "total_agb_co2_stock": "Total AGB CO₂ Stock (kg)",
+    "total_bgb_co2_stock": "Total BGB CO₂ Stock (kg)"
+}
+
+POTENTIAL_FIELDS = {
+    "mean": "Average",
+    "standard_deviation": "Standard Deviation"
+}
+
+AGB_DENSITY_FIELDS = {
+    "total_agb_density": "Total AGB Density (Mg/ha)",
+    "total_bgb_density": "Total BGB Density (Mg/ha)",
+    "total_agb_c_stock_density": "Total AGB C Stock Density (Mg/ha)",
+    "total_bgb_c_stock_density": "Total BGB C Stock Density (Mg/ha)",
+    "total_agb_co2_stock_density": "Total AGB CO₂ Stock Density (Mg/ha)",
+    "total_bgb_co2_stock_density": "Total BGB CO₂ Stock Density (Mg/ha)"
+}
+
+VECTOR_FIELDS = {
+    "BRGY": {
+        "AGB": {"barangay": "Barangay"} | AGB_FIELDS,
+        "POTENTIAL": {"barangay": "Barangay"} | POTENTIAL_FIELDS,
+        "AGB_DENSITY": {"barangay": "Barangay"} | AGB_DENSITY_FIELDS,
+    },
+    "DIST": {
+        "AGB": {"district": "District"} | AGB_FIELDS,
+        "POTENTIAL": {"district": "District"} | POTENTIAL_FIELDS,
+        "AGB_DENSITY": {"district": "District"} | AGB_DENSITY_FIELDS,
+    }
+}
+
+VECTOR_FIELDS_COLORED = {
+    "AGB": "total_agb",
+    "POTENTIAL": "mean",
+    "AGB_DENSITY": "total_agb_density",
+}
--- sequestree.py
+++ sequestree.py
@@ -1,99 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 import streamlit as st
 from data_resolver import DataResolver, LocalDataResolver
 import rasters_view
 import vectors_view
+from config import *
 
 st.title("Spatial Assessment and Backcasting of Tree Carbon Sequestration (CS) in Quezon City, Philippines")
 
-choices = [
-    "Tree Biomass and Carbon Stock of Quezon City, Philippines - Random Forest Predictions",
-    "Tree Biomass and Carbon Stock of Quezon City, Philippines - Geographically Weighted Regression Predictions",
-    "Tree Carbon Sequestration Potential of Quezon City, Philippines - Random Forest Predictions",
-    "Tree Carbon Sequestration Potential of Quezon City Philippines - Geographically Weighted Regression Predictions",
-    "Tree Biomass and Carbon Stock of Quezon City, Philippines Per Zone - Random Forest Predictions",
-    "Tree Biomass and Carbon Stock of Quezon City, Philippines Per Zone - Geographically Weighted Regression Predictions",
-    "Tree Carbon Sequestration Potential of Quezon City, Philippines Per Zone - Random Forest Predictions",
-    "Tree Carbon Sequestration Potential of Quezon City, Philippines Per Zone - Geographically Weighted Regression Predictions"
-]
-
 # options
 data_resolver = DataResolver(LocalDataResolver())
-
-AGB_DATA_TYPE_MAP = {
-    "AGB": "Aboveground Biomass",
-    "POTENTIAL": "Tree Carbon Sequestration Potential"
-}
-
-ZONE_DATA_TYPE_MAP = {
-    "AGB": "Tree Biomass and Carbon Stock",
-    "AGB_DENSITY": "Tree Biomass and Carbon Stock Density",
-    "POTENTIAL": "Tree Carbon Sequestration Potential"
-}
-
-MODEL_MAP = {
-    "RF": "Random Forest",
-    "HYBRID": "Hybrid"
-}
-
-VIEW_TYPE_MAP = {
-    "BRGY": "Barangay",
-    "DIST": "District"
-}
-
-OPTION_MAP = {
-    "RASTERS": "Raw Maps",
-    "VECTORS": "Zonal Maps"
-}
-
-AGB_FIELDS = {
-    "average_agb": "Average AGB (kg)",
-    "standard_deviation": "Standard Deviation (kg)",
-    "total_agb": "Total AGB (kg)",
-    "total_bgb": "Total BGB (kg)",
-    "total_agb_c_stock": "Total AGB C Stock (kg)",
-    "total_bgb_c_stock": "Total BGB C Stock (kg)",
-    "total_agb_co2_stock": "Total AGB CO₂ Stock (kg)",
-    "total_bgb_co2_stock": "Total BGB CO₂ Stock (kg)"
-}
-
-POTENTIAL_FIELDS = {
-    "mean": "Mean (kg)",
-    "standard_deviation": "Standard Deviation (kg)"
-}
-
-AGB_DENSITY_FIELDS = {
-    "total_agb_density": "Total AGB Density (Mg/ha)",
-    "total_bgb_density": "Total BGB Density (Mg/ha)",
-    "total_agb_c_stock_density": "Total AGB C Stock Density (Mg/ha)",
-    "total_bgb_c_stock_density": "Total BGB C Stock Density (Mg/ha)",
-    "total_agb_co2_stock_density": "Total AGB CO₂ Stock Density (Mg/ha)",
-    "total_bgb_co2_stock_density": "Total BGB CO₂ Stock Density (Mg/ha)"
-}
-
-VECTOR_FIELDS = {
-    "BRGY": {
-        "AGB": {"barangay": "Barangay"} | AGB_FIELDS,
-        "POTENTIAL": {"barangay": "Barangay"} | POTENTIAL_FIELDS,
-        "AGB_DENSITY": {"barangay": "Barangay"} | AGB_DENSITY_FIELDS,
-    },
-    "DIST": {
-        "AGB": {"district": "District"} | AGB_FIELDS,
-        "POTENTIAL": {"district": "District"} | POTENTIAL_FIELDS,
-        "AGB_DENSITY": {"district": "District"} | AGB_DENSITY_FIELDS,
-    }
-}
 
 def map_to_value(dict):
     def _do_stuff(input):
         return dict[input]
     return _do_stuff
 
-YEARS = ["2020", "2021", "2022", "2023", "2024"]
+def build_year_dict(ext):
+    return {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
 
 option_selected = st.sidebar.selectbox("Choose:", list(OPTION_MAP.keys()), format_func=map_to_value(OPTION_MAP))
 if option_selected == "RASTERS":
@@ -105,14 +27,12 @@ if option_selected == "RASTERS":
     folder_name = f"{option_selected}/{data_type}_{model}/"
 
     ext = ".tif"
-    result = {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
+    files = build_year_dict(ext)
 
-    if data_type == "AGB":
-        rasters_view.run(result, "Aboveground Biomass (kg)", 0, 2500)
-    elif data_type == "POTENTIAL":
-        rasters_view.run(result, "Tree Carbon Sequestration Potential", 0, 2)
-    else:
-        raise TypeError("Unknown data type: " + data_type)
+    min, max = AGB_DATA_TYPE_MIN_MAX[data_type]
+    caption = AGB_DATA_TYPE_CAPTION[data_type]
+
+    rasters_view.run(files, caption, min, max)
 elif option_selected == "VECTORS":
     data_type = st.sidebar.selectbox("Data Type", list(ZONE_DATA_TYPE_MAP.keys()), format_func=map_to_value(ZONE_DATA_TYPE_MAP))
     view_type = st.sidebar.selectbox("View Type", list(VIEW_TYPE_MAP.keys()), format_func=map_to_value(VIEW_TYPE_MAP))
@@ -121,18 +41,9 @@ elif option_selected == "VECTORS":
     folder_name = f"{option_selected}/{data_type}_{model}_{view_type}/"
 
     ext = ".gpkg"
-    result = {year: data_resolver.resolve_file(folder_name + year + ext) for year in YEARS}
+    files = build_year_dict(ext)
 
     fields = VECTOR_FIELDS[view_type][data_type]
-    field_colored = ""
-    match data_type:
-        case "AGB":
-            field_colored = "total_agb"
-        case "POTENTIAL":
-            field_colored = "mean"
-        case "AGB_DENSITY":
-            field_colored = "total_agb_density"
-        case _:
-            raise TypeError("Should NOT happen!")
+    field_colored = VECTOR_FIELDS_COLORED[data_type]
 
-    vectors_view.run(result, field_colored, fields)
+    vectors_view.run(files, field_colored, fields)
