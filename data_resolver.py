93e8c309458a47c3e59b5d02ec5fdb6a36dc6069
brickwall2900 <brickwall2900@gmail.com>
2025-10-21 17:20:52 +0800

    remove comment on LocalDataResolver

--- data_resolver.py
+++ data_resolver.py
@@ -15,9 +15,5 @@ except:
 
 class LocalDataResolver:
     def resolve_file(self, filename):
-        # Our working directory is...
-        # F:\UserData\sequestree\sequestree_database
-        # here!
-
         file_path = BASE_DIR + "/" + filename
         return file_path
